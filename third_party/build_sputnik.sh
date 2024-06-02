7e37b89a1daac0794b182f3a878b362c020db17d
fan1997 <2479031300@qq.com>
2024-06-02 17:14:33 +0800

    Fix build_sputnik.sh

--- third_party/build_sputnik.sh
+++ third_party/build_sputnik.sh
@@ -1,6 +1,7 @@
-cd ./glog && mkdir build && cd build
-cmake ..
-make -j 
+cd ${DTC_HOME}/third_party/glog  && mkdir build && cd build
+cmake -DCMAKE_INSTALL_PREFIX=${DTC_HOME}/third_party/glog/build ..
+make -j
+make install 
 
 GlogPath="${DTC_HOME}/third_party/glog"
 #setting up GLOG environment
@@ -11,14 +12,14 @@ then
 else
   echo "Setting up Glog environment..."
   export GLOG_PATH=$GlogPath
-  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$GLOG_PATH/build
-  export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:$GLOG_PATH/build/glog
-  export LIBRARY_PATH=$LD_LIBRARY_PATH:$GLOG_PATH/build
+  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$GLOG_PATH/build/lib
+  export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:$GLOG_PATH/build/include
+  export LIBRARY_PATH=$LD_LIBRARY_PATH:$GLOG_PATH/build/lib
 fi
 
 
-cd ./sputnik && mkdir build && cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_TEST=OFF -DBUILD_BENCHMARK=OFF -DCUDA_ARCHS="89;86"
+cd ${DTC_HOME}/third_party/sputnik  && mkdir build && cd build
+cmake .. -DGLOG_INCLUDE_DIR=$GLOG_PATH/build/include -DGLOG_LIBRARY=$GLOG_PATH/build/lib/libglog.so -DCMAKE_BUILD_TYPE=Release -DBUILD_TEST=OFF -DBUILD_BENCHMARK=OFF -DCUDA_ARCHS="89;86"
 make -j12 
 
 
